3e4baee2d3f18ec5149796fb2bceef62b3b6feeb
raroa <darkkcity@yahoo.com>
2017-11-14 14:12:58 +0200

    More components

---
+++ src/Components/MainIndex/MainIndex.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+
+export class MainIndex extends Component {
+    render() {
+        return (
+            <div className="nk-portfolio-list nk-isotope nk-isotope-3-cols nk-isotope-gap">
+            
+            
+                            <div className="nk-isotope-item" data-filter="Mockup">
+                                <div className="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+                                    <a href="work-single-3.html" className="nk-portfolio-item-link"></a>
+                                    <div className="nk-portfolio-item-image">
+                                        <div style={{backgroundImage: `url('assets/images/portfolio-7-sm.jpg')`}}></div>
+                                    </div>
+                                    <div className="nk-portfolio-item-info nk-portfolio-item-info-top">
+                                        <div>
+                                            <h2 className="portfolio-item-title h3">Vinyl Record</h2>
+                                            <div className="portfolio-item-category">Mockup</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
+            
+                            <div className="nk-isotope-item" data-filter="Print">
+                                <div className="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+                                    <a href="work-single-4.html" className="nk-portfolio-item-link"></a>
+                                    <div className="nk-portfolio-item-image">
+                                        <div style={{backgroundImage: `url('assets/images/portfolio-4-sm.jpg')`}}></div>
+                                    </div>
+                                    <div className="nk-portfolio-item-info nk-portfolio-item-info-top">
+                                        <div>
+                                            <h2 className="portfolio-item-title h3">Modern T-Shirt</h2>
+                                            <div className="portfolio-item-category">Print</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
+            
+                            <div className="nk-isotope-item" data-filter="Branding">
+                                <div className="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-lg">
+                                    <a href="work-single-5.html" className="nk-portfolio-item-link"></a>
+                                    <div className="nk-portfolio-item-image">
+                                        <div style={{backgroundImage: `url('assets/images/portfolio-5-sm.jpg')`}}></div>
+                                    </div>
+                                    <div className="nk-portfolio-item-info nk-portfolio-item-info-top">
+                                        <div>
+                                            <h2 className="portfolio-item-title h3">Minimal Bag</h2>
+                                            <div className="portfolio-item-category">Branding</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
+            
+                            <div className="nk-isotope-item" data-filter="Design">
+                                <div className="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+                                    <a href="work-single-3.html" className="nk-portfolio-item-link"></a>
+                                    <div className="nk-portfolio-item-image">
+                                        <div style={{backgroundImage: `url('assets/images/portfolio-9-sm.jpg')`}}></div>
+                                    </div>
+                                    <div className="nk-portfolio-item-info nk-portfolio-item-info-top">
+                                        <div>
+                                            <h2 className="portfolio-item-title h3">Clean &amp; Groovy</h2>
+                                            <div className="portfolio-item-category">Design</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
+            
+                            <div className="nk-isotope-item" data-filter="Design">
+                                <div className="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+                                    <a href="work-single-3.html" className="nk-portfolio-item-link"></a>
+                                    <div className="nk-portfolio-item-image">
+                                        <div style={{backgroundImage: `url('assets/images/portfolio-3-sm.jpg')`}}></div>
+                                    </div>
+                                    <div className="nk-portfolio-item-info nk-portfolio-item-info-top">
+                                        <div>
+                                            <h2 className="portfolio-item-title h3">Minimal Mobile App</h2>
+                                            <div className="portfolio-item-category">Design</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
+            
+                            <div className="nk-isotope-item" data-filter="Print">
+                                <div className="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-lg">
+                                    <a href="work-single-3.html" className="nk-portfolio-item-link"></a>
+                                    <div className="nk-portfolio-item-image">
+                                        <div style={{backgroundImage: `url('assets/images/portfolio-8-sm.jpg')`}}></div>
+                                    </div>
+                                    <div className="nk-portfolio-item-info nk-portfolio-item-info-top">
+                                        <div>
+                                            <h2 className="portfolio-item-title h3">White T-Shirt</h2>
+                                            <div className="portfolio-item-category">Print</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
+            
+                            <div className="nk-isotope-item" data-filter="Photography">
+                                <div className="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-lg">
+                                    <a href="work-single-1.html" className="nk-portfolio-item-link"></a>
+                                    <div className="nk-portfolio-item-image">
+                                        <div style={{backgroundImage: `url('assets/images/portfolio-6-sm.jpg')`}}></div>
+                                    </div>
+                                    <div className="nk-portfolio-item-info nk-portfolio-item-info-top">
+                                        <div>
+                                            <h2 className="portfolio-item-title h3">Heja Stockholm</h2>
+                                            <div className="portfolio-item-category">Photography</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
+            
+                            <div className="nk-isotope-item" data-filter="Branding">
+                                <div className="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+                                    <a href="work-single-2.html" className="nk-portfolio-item-link"></a>
+                                    <div className="nk-portfolio-item-image">
+                                        <div style={{backgroundImage: `url('assets/images/portfolio-2-sm.jpg')`}}></div>
+                                    </div>
+                                    <div className="nk-portfolio-item-info nk-portfolio-item-info-top">
+                                        <div>
+                                            <h2 className="portfolio-item-title h3">Business Card</h2>
+                                            <div className="portfolio-item-category">Branding</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
+            
+                            <div className="nk-isotope-item" data-filter="Photography">
+                                <div className="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+                                    <a href="work-single-3.html" className="nk-portfolio-item-link"></a>
+                                    <div className="nk-portfolio-item-image">
+                                        <div style={{backgroundImage: `url('assets/images/portfolio-12-sm.jpg')`}}></div>
+                                    </div>
+                                    <div className="nk-portfolio-item-info nk-portfolio-item-info-top">
+                                        <div>
+                                            <h2 className="portfolio-item-title h3">Paper Bag</h2>
+                                            <div className="portfolio-item-category">Photography</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
+                        </div>
+        );
+    }
+}
